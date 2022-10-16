555a5c47f21167f7c39c9b4f87f163407c862f00
Kamolov Amir <kamolov.amir2000@yandex.ru>
2022-10-16 23:27:24 +0300

    [+] начали проект

---
+++ webService.WebApi/Human.cs
@@ -0,0 +1,22 @@
+﻿namespace webService.WebApi;
+
+public class Human
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Gender Gender { get; set; }
+    public List<Hobby> Hobbies { get; set; }
+}
+
+public class Hobby
+{
+    public string Name { get; set; }
+}
+
+public enum Gender
+{
+    None,
+    Man,
+    Woman,
+    Helicopter
+}
