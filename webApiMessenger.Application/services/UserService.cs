a943ba48f97fe0acf9a8ff39e13428a185354f0e
Ivan_Glushchenko <poliantsevivan@gmail.com>
2022-10-29 22:52:22 +0300

    Добвление в друзья

---
+++ webApiMessenger.Application/services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using webApiMessenger.Domain.Repositories;
+
+namespace webApiMessenger.Application.services
+{
+    public class UserService
+    {
+        private UserRepository _userRepository;
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+        public void AddFriend(int user1id, int user2id) 
+        {
+            _userRepository.AddFriend(user1id, user2id);
+
+        }
+    }
+}
