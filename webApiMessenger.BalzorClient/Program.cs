866cfaf9fc45772163fd80fedb94733e6dc11d7e
Kamolov Amir <kamolov.amir2000@yandex.ru>
2023-04-23 00:13:46 +0300

    [+] авторизацию и аунтефикацию на стороне клиента

--- webApiMessenger.BalzorClient/Program.cs
+++ webApiMessenger.BalzorClient/Program.cs
@@ -1,13 +1,23 @@
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using webApiMessenger.BalzorClient;
 using webApiMessenger.BalzorClient.Options;
+using webApiMessenger.BalzorClient.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddOptions<ServerSettings>()
     .Bind(builder.Configuration.GetSection(ServerSettings.ConfigName))
     .ValidateDataAnnotations()
     .ValidateOnStart();
+
+builder.Services.AddBlazoredSessionStorageAsSingleton();
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+
+builder.Services.AddSingleton<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddAuthorizationCore();
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
---
+++ webApiMessenger.BalzorClient/Services/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace webApiMessenger.BalzorClient.Services;
+
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly ILocalStorageService _localStorageService;
+    private readonly ISessionStorageService _sessionStorageService;
+
+    public CustomAuthenticationStateProvider(ILocalStorageService localStorageService, ISessionStorageService sessionStorageService)
+    {
+        _localStorageService = localStorageService ?? throw new ArgumentNullException(nameof(localStorageService));
+        _sessionStorageService = sessionStorageService ?? throw new ArgumentNullException(nameof(sessionStorageService));
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var jwt = await _localStorageService.GetItemAsStringAsync("jwt");
+        if (jwt == null)
+        {
+            jwt = await _sessionStorageService.GetItemAsStringAsync("jwt");
+        }
+        var claimIdentity = new ClaimsIdentity();
+
+        if (jwt != null)
+        {
+            claimIdentity = new ClaimsIdentity(ParseClaimsFromJwt(jwt), "jwt");
+        }
+
+        var userClaimPrincipal = new ClaimsPrincipal(claimIdentity);
+        var state = new AuthenticationState(userClaimPrincipal);
+        NotifyAuthenticationStateChanged(Task.FromResult(state));
+        return state;
+    }
+
+    public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var payload = jwt.Split('.')[1];
+        var jsonBytes = ParseBase64WithoutPadding(payload);
+        var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+        return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+    }
+
+    private static byte[] ParseBase64WithoutPadding(string base64)
+    {
+        switch (base64.Length % 4)
+        {
+            case 2: base64 += "=="; break;
+            case 3: base64 += "="; break;
+        }
+        return Convert.FromBase64String(base64);
+    }
+}
